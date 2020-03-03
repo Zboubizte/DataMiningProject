bc0a13375540498bdfd4470e89953eef5b851ffc
Zboubizte <ken.bres@hotmail.fr>
2020-03-03 12:30:46 +0100

    Correction d'une erreur de calcul

--- Kowalski.py
+++ Kowalski.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # Chargement des datasets
 df_demissionnaire = pd.read_csv("donnees/data_mining_DB_clients_tbl.csv",
-                    usecols = ["CDSEXE", "NBENF", "CDSITFAM", "DTADH", "CDTMT", "CDCATCL", "agedem"])
+                    usecols = ["CDSEXE", "NBENF", "CDSITFAM", "CDTMT", "CDCATCL", "agedem", "adh"])
 df_random = pd.read_csv("donnees/data_mining_DB_clients_tbl_bis.csv",
                     usecols = ["CDSEXE", "NBENF", "CDSITFAM", "DTADH", "CDTMT", "CDCATCL", "CDMOTDEM", "DTDEM", "DTNAIS"])
 
@@ -13,8 +13,9 @@ df_demissionnaire["demissionnaire"] = True
 df_random["demissionnaire"] = np.where(df_random["CDMOTDEM"].notnull(), True, False)
 #df_random = df_random.drop(columns = "CDMOTDEM")
 
-# On renomme la colonne agedem en age
+# On renomme les colonnes
 df_demissionnaire = df_demissionnaire.rename(columns = {"agedem": "age"})
+df_demissionnaire = df_demissionnaire.rename(columns = {"adh": "duree"})
 
 # On supprime les lignes contenant des erreurs de saisies.
 # C'est à dire les démissionnaires n'ayant pas de date de démission ou les dates de naissances vides
@@ -22,24 +23,24 @@ df_random = df_random.drop(df_random[(df_random["CDMOTDEM"] == "DC") & (df_rando
 df_random = df_random.drop(df_random[df_random["DTNAIS"] == "0000-00-00"].index)
 
 # On calcule l'age de démission, ou l'age si il n'y a pas de démission dans le df_random
-df_random.loc[df_random["demissionnaire"] == True, "age"] = df_random["DTDEM"].str.slice(stop = 4).astype(int) - df_random["DTADH"].str.slice(stop = 4).astype(int)
+df_random.loc[df_random["demissionnaire"] == True, "age"] = df_random["DTDEM"].str.slice(stop = 4).astype(int) - df_random["DTNAIS"].str.slice(stop = 4).astype(int)
 df_random.loc[df_random["demissionnaire"] == False, "age"] = 2007 - df_random["DTNAIS"].str.slice(stop = 4).astype(int)
 
+# On calcule la durée d'adhésion (jusqu'à démission si démission)
+df_random.loc[df_random["demissionnaire"] == True, "duree"] = df_random["DTDEM"].str.slice(stop = 4).astype(int) - df_random["DTADH"].str.slice(stop = 4).astype(int)
+df_random.loc[df_random["demissionnaire"] == False, "duree"] = 2007 - df_random["DTADH"].str.slice(stop = 4).astype(int)
+
 # On ordonne les colonnes de la même façon
-df_demissionnaire = df_demissionnaire[["CDSEXE", "NBENF", "CDSITFAM", "DTADH", "CDTMT", "CDCATCL", "age", "demissionnaire"]]
-df_random = df_random[["CDSEXE", "NBENF", "CDSITFAM", "DTADH", "CDTMT", "CDCATCL", "age", "demissionnaire"]]
+df_demissionnaire = df_demissionnaire[["CDSEXE", "NBENF", "CDSITFAM", "CDTMT", "CDCATCL", "age", "duree", "demissionnaire"]]
+df_random = df_random[["CDSEXE", "NBENF", "CDSITFAM", "CDTMT", "CDCATCL", "age", "duree", "demissionnaire"]]
 
 # On concatène les deux datasets
 df = pd.concat([df_demissionnaire, df_random], ignore_index = True)
 df["age"] = df["age"].astype(int)
+df["duree"] = df["duree"].astype(int)
 
 # La colonne CDSITFAM est groupée par des lettres décrivant la situation familiale
 # Remplacement de ces lettres par les numéro de lettre correspondant
 df["CDSITFAM"] = df["CDSITFAM"].apply(lambda x: ord(x.lower()) - 96).astype(int)
 
-# Transformation de date d'adhésion en durée d'adhésion
-df["DTADH"] = 2007 - df["DTADH"].str.slice(stop = 4).astype(int)
-df = df.rename(columns = {"DTADH": "dureeadh"})
-
 print df.head(1)
-print df.dtypes
